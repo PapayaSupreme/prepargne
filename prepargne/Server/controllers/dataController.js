aa9de34d0d9700e359ef811adb2afae1cf30df97
Herschel_TC <raphael.lesterlin@gmail.com>
2026-04-18 21:28:33 +0200

    finished GET request, ready to merge

--- prepargne/Server/controllers/dataController.js
+++ prepargne/Server/controllers/dataController.js
@@ -246,5 +246,47 @@ exports.updateSpending = (req, res) => {
 }
 
 exports.retrieveData = (req, res) => { //The get that will get all the precious data
-    res.status(200).send({ message: "WIP." })
+    if (req.body === undefined) {
+        return res.status(400).send({ message : "Empty request body." })
+    }
+    if (req.body.token === undefined) {
+        return res.status(400).send({ message : "Missing parameters." })
+    }
+    jwt.verify(req.body.token, config.secret, (err, decoded) => { 
+        if (err) { 
+            return res.status(401).send({ 
+                message: "Unauthorized." 
+            }); 
+        }
+
+        var hashed = crypto.createHmac('sha256', config.secret).update(decoded.id + decoded.username, 'utf8').digest('hex')
+        var tokendata = "cara istouille"
+        var spendingdata = []
+
+        Tokenized.findOne({
+            where: { hash: hashed }
+        }).then(record => {
+            console.log(record.dataValues)
+            tokendata = record.dataValues
+
+            Spending.findAll({
+                where: { hash: hashed }
+            }).then(record => {
+                console.log(record)
+                if (record) {
+                    record.forEach(element => {
+                        spendingdata.push(element.dataValues)
+                    });
+                }
+
+                res.status(200).send({ message: "Fetching completed, data sent in body", data: { UserKeyInfo: tokendata, Spending: spendingdata } })
+            }).catch(err => {
+                return res.status(500).send({ message: "Exception has occurred during Spending fetching : " + err.message })
+            })
+
+        }).catch(err => {
+            return res.status(500).send({ message: "Exception has occurred during Tokenized fetching : " + err.message })
+        })
+
+    })
 }
